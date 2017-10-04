ebe6811ab084b54fbae6a54eae2a7ac5be91cd00
Bram van Osta <bvanosta@clever-age.net>
2017-10-04 14:47:16 +0200

    :lipstick: Adds responsive to footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <div class="footer__container container">
-      <div class="footer__left-footer">Copyright © 2017</div>
+      <div class="footer__left-column">Copyright © 2017</div>
       <div class="footer__right-column">Last updated at: {{updatedAt}}</div>
     </div>
   </footer>
@@ -22,6 +22,7 @@
 
 <style scoped>
   .footer {
+    margin-top: 75px;
     background-color: #262525;
     color: #ffffff;
   }
@@ -31,5 +32,21 @@
     align-items: center;
     justify-content: space-between;
     padding: 10px 25px;
+  }
+
+  @media (max-width: 800px) {
+    .footer {
+      margin-top: 50px;
+    }
+  }
+
+  @media (max-width: 600px) {
+    .footer__container {
+      flex-direction: column
+    }
+
+    .footer__left-column {
+      margin-bottom: 15px;
+    }
   }
 </style>
