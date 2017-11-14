2dac6909fb5060b3dde59200fc00bd60fb117528
Bram van Osta <bramvanosta@gmail.com>
2017-11-14 08:36:28 +0100

    :lipstick: Small UI tweaks on directionItems

--- src/components/core/DirectionItem.vue
+++ src/components/core/DirectionItem.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="direction__item">
-    <p class="direction__information" v-if="hours > 0 && minutes > 0">
-      <span class="direction__information-label">Duration</span>
-      <span class="direction__information-value">{{ hours > 0 ? `${hours}h ` : '' }}{{ minutes > 0 ? `${minutes}min` : '' }}</span>
-    </p>
-    <p class="direction__information" v-if="kilometers > 0">
-      <span class="direction__information-label">Distance</span>
-      <span class="direction__information-value">{{ kilometers }} km</span>
-    </p>
+    <div class="direction__header">
+      <p class="direction__information" v-if="minutes > 1">
+        <span class="direction__information-label">
+          <img src="../../assets/icons/duration.svg" alt="Duration icon" />
+        </span>
+        <span class="direction__information-value">{{ hours > 0 ? `${hours}h ` : '' }}{{ minutes > 0 ? `${minutes}min` : '' }}</span>
+      </p>
+      <p class="direction__information" v-if="kilometers > 1">
+        <span class="direction__information-label">
+          <img src="../../assets/icons/distance.svg" alt="Distance icon" />
+        </span>
+        <span class="direction__information-value">{{ kilometers }} km</span>
+      </p>
+    </div>
     <p class="direction__instruction" v-html="step.instruction" />
   </div>
 </template>
@@ -55,12 +61,26 @@
     border-bottom: 1px solid rgba(0, 0, 0, .1);
   }
 
+  .direction__header {
+    display: flex;
+    align-items: center;
+  }
+
+  .direction__header .direction__information:not(:last-of-type) {
+    margin-right: 15px;
+  }
+
   .direction__information {
+    display: flex;
+    align-items: center;
     margin-bottom: 10px;
   }
 
   .direction__information-label {
-    font-weight: bold;
+    display: flex;
+    align-items: center;
+    width: 20px;
+    margin-right: 8px;
   }
 
   .direction__instruction {
