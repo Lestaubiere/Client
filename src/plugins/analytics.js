fbff31ade809e7cb343304ffaa5001f5374529e0
Bram van Osta <bramvanosta@gmail.com>
2017-12-01 20:54:16 +0100

    :chart_with_upwards_trend: Updates GA code to production code

--- src/plugins/analytics.js
+++ src/plugins/analytics.js
@@ -4,7 +4,7 @@ import VueAnalytics from 'vue-analytics';
 import router from '@/router';
 
 const analytics = Vue.use(VueAnalytics, {
-  id: 'UA-39908849-3',
+  id: 'UA-39908849-2',
   router,
 });
 
