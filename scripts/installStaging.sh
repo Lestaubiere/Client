1a870da94d9ed9a91a16e7e587674eb12b4763b3
Bram van Osta <bramvanosta@gmail.com>
2017-12-01 20:58:21 +0100

    :rocket: Sets staging deployment branch to development

--- scripts/installStaging.sh
+++ scripts/installStaging.sh
@@ -10,7 +10,7 @@ echo "npm: `npm -v`"
 # Get the latest version via git
 echo "### Getting the latest version via git ###"
 git fetch origin
-git reset origin/master --hard
+git reset origin/development --hard
 
 # Install Front-end dependencies for Vue
 echo "### Installing Front-end depenencies for Vue ###"
