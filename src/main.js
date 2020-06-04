72334c2cc31542594a895954ce1f1393d0eaa6ae
Bram van Osta <bramvanosta@gmail.com>
2020-06-04 19:13:41 +0200

    :sparkles: Update website with new safaritent info

--- src/main.js
+++ src/main.js
@@ -11,12 +11,21 @@ import i18n from '@/plugins/i18n';
 
 Vue.config.productionTip = false;
 
+if (window.navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+}
+
 /* eslint-disable no-new */
+/* eslint arrow-parens: ["error", "always"] */
 new Vue({
   el: '#app',
   router,
   store,
   analytics,
   i18n,
-  render: h => h(App),
+  render: (h) => h(App),
 });
